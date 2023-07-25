eea0ec50b6f7d9704d2df4ad3eed8aac24c66abb
AprilHappinessweb <44639660+AprilHappinessweb@users.noreply.github.com>
2023-07-25 10:49:36 +0800

    博客
    博客

--- dist/assets/column_project_index.md.a92393b8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./chunks/framework.7ca433f4.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"column/project/index.md","filePath":"column/project/index.md"}'),n={name:"column/project/index.md"};function c(r,a,s,d,i,p){return t(),o("div")}const l=e(n,[["render",c]]);export{_ as __pageData,l as default};
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,5 +18,6 @@ export default defineConfig({
       provider: 'local'
     }
   },
-  base:'/blog_vitepress/'
+  base:'/blog_vitepress/',
+  outDir:'../blog_vitepress'
 })
